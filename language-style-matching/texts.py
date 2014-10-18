68183c16c0f816c0e16f94c37b7f3f7ae90cdfea
Alex Spinnler <SpinnleA@advisory.com>
2014-10-18 12:03:24 -0500

    parse timestamp of message

--- language-style-matching/texts.py
+++ language-style-matching/texts.py
@@ -1,6 +1,6 @@
+import re
 from datetime import datetime
 from initialize import *
-
 
 
 sent_prefix = "Sent on"
@@ -10,6 +10,10 @@ is_sent = lambda s: s.startswith( sent_prefix )
 is_received = lambda s: s.startswith( received_prefix )
 
 class Text(object):
+    
+    @staticmethod
+    def parse_timestamp(string):
+        return datetime.strptime( string, "%b %d %Y %I:%M:%S %p" )
     
     @staticmethod
     def process(sender, filename):
@@ -29,6 +33,12 @@ class Text(object):
                 if ( is_sent(line) ):
                     direction = Direction.Sent
                 
+                    # parse timestamp
+                    replacements = [ ",", sent_prefix ]
+                    # batch string replace with ""
+                    line = re.sub( "|".join( replacements ), "", line).strip()
+                    timestamp = Text.parse_timestamp( line )
+                    
                     # skip ahead to next line
                     line = texts.next()
                     
@@ -37,6 +47,12 @@ class Text(object):
                 
                 elif ( is_received(line) ):
                     direction = Direction.Received
+                    
+                    # parse timestamp
+                    replacements = [ ",", received_prefix ]
+                    # batch string replace with ""
+                    line = re.sub( "|".join( replacements ), "", line).strip()
+                    timestamp = Text.parse_timestamp( line )
                     
                     # skip ahead to next line
                     line = texts.next()
@@ -81,7 +97,7 @@ if __name__ == "__main__":
     with open("received.txt", "w") as received_file:
         received_file.write("\n".join( [ msg.message for msg in received ] ))
         
-    # populating DB
+    # populate DB
     for msg in sent:
         DB.write( msg )
     
