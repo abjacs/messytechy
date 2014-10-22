92b03352a4a05853672df4d96a0d7d8242a3575b
Alex Spinnler <SpinnleA@advisory.com>
2014-10-22 00:42:37 -0500

    start defining remaining code to find LSMs across sender/receivers and desired time periods. next up: graphing computed LSMs

---
+++ language-style-matching/lsm.py
@@ -0,0 +1,35 @@
+
+if __name__ == "__main__":
+    """
+    - parse texts to DB
+    - iterate through and compute LSM for a given
+        - time period
+        - sender/receiver
+    - persist computed valued to DB table: LSM
+        (lsm, sender, receiver, startdate, enddate)
+    """
+    
+    
+    tasks = [
+        
+    ]
+    
+
+def get_sender_receiver_pairs():
+    # i.e. "(alex, kit kat, start_date, end_date), (alex, lily carter, start_date, end_date)"
+    return []
+    
+def compute_lsm(text_1, text_2):
+    return 1.0
+    
+def get_breakdowns(start_date, end_date):
+    """
+        - days
+        - weeks
+        - months
+    """
+    return {
+        (start_date, end_date) : [  ]
+    }
+
+
