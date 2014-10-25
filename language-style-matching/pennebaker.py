6ebe493a9b49ce259b2e3427bf383173d2228abc
Alex Spinnler <alex.spinnler@gmail.com>
2014-10-25 01:00:08 -0500

    add methods to 'anonymize' text to match what front end code does on Pennebaker website before form is posted and send to 'backend'

--- language-style-matching/pennebaker.py
+++ language-style-matching/pennebaker.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import requests
 import re
+import random
 
 
 class Api(object):
@@ -18,7 +20,9 @@ class Api(object):
             - return LSM
         """
         
+        """
         tasks = [
+            Api.__prepare,
             Api.__make_request,
             Api.__parse_lsm
         ]
@@ -28,9 +32,27 @@ class Api(object):
         resp = start(text_1, text_2)
         
         for task in tasks[ 1: ]:
-            resp = task( resp )
+            print "%s with argument of type: %s" % (task.func_name, type(resp))
+            resp = task( *resp )
         
         return resp
+        """
+        
+        (text_1, text_2) = Api.__prepare(text_1, text_2)
+        
+        html = Api.__make_request(text_1, text_2)
+        
+        lsm = Api.__parse_lsm(html)
+        
+        return lsm
+        
+
+    @staticmethod
+    def __prepare(text_1, text_2):
+        text_1 = Api.__anonymize(text_1)
+        text_2 = Api.__anonymize(text_2)
+        
+        return (text_1, text_2)
     
     @staticmethod
     def __make_request(text_1, text_2):
@@ -88,15 +110,25 @@ class Api(object):
         return resp.content
     
     @staticmethod
+    def __random_order():
+    	return round(random.random()) - 0.5
+        
+    @staticmethod
+    def __anonymize(msg):
+        messages = msg.split(" ")
+        random.shuffle(messages, Api.__random_order)
+        
+        return " ".join( messages )
+    
+    @staticmethod
     def __parse_lsm(raw_html):
-        pattern = "Your LSM score is \d+.\d+"
-        
+        pattern = "Your LSM score is (\d+.\d+|\d)"
         match = re.search(pattern, raw_html)
-        
+    
         if match:
             # number should be at end of string
             lsm = match.group(0).split()[-1:].pop()
-        
+    
             return lsm
         else:
             raise Exception("Could not parse LSM from source text using pattern '%s'" % pattern)
