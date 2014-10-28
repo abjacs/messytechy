88abedabe8200671c02dfd32d20156c87a766af4
Alex Spinnler <alex.spinnler@gmail.com>
2014-10-28 00:58:35 -0500

    cleanup

--- language-style-matching/lsm.py
+++ language-style-matching/lsm.py
@@ -7,8 +7,6 @@ import pennebaker
 
 
 def get_sender_receiver_pairs():
-    # i.e. "(alex, kit kat, start_date, end_date), (alex, lily carter, start_date, end_date)"
-    
     query = """
         SELECT
             Sender,
@@ -19,31 +17,9 @@ def get_sender_receiver_pairs():
     """
     
     return DB.query( query )
-    
-def compute_lsm(text_1, text_2):
-    return 1.0
-    
-def get_breakdowns(start_date, end_date):
-    """
-        - days
-        - weeks
-        - months
-    """
-    return {
-        (start_date, end_date) : [  ]
-    }
 
 
 if __name__ == "__main__":
-    """
-    - parse texts to DB
-    - iterate through and compute LSM for a given
-        - time period
-        - sender/receiver
-    - persist computed valued to DB table: LSM
-        (lsm, sender, receiver, startdate, enddate)
-    """
-    
     senders_receivers = get_sender_receiver_pairs()
     
     text_aggregates = []
